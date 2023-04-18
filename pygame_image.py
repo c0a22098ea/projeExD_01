54f869f167af03f9263f7a38b1fbf3d7def8e3a0
Shusuke.C <c0a22098ea@edu.teu.ac.jp>
2023-04-18 16:11:58 +0900

    練習問題5

--- pygame_image.py
+++ pygame_image.py
@@ -8,7 +8,8 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
-    kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
+    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img , 10, 1.0)]
+
     tmr = 0
     
 
@@ -18,8 +19,9 @@ def main():
             if event.type == pg.QUIT: return
 
         tmr += 1
+        screen.blit(bg_img ,[-tmr , 0] )
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [100, 200])
+        screen.blit(kk_imgs[tmr%2], [300, 200])
 
         pg.display.update()
         clock.tick(100)
